a933a65f62f6dd3eed3d46737dc56326178cee67
jali-k <e19210@eng.pdn.ac.lk>
2024-10-19 22:27:34 +0530

    Temporarily comment out appoinment urls

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("schedule/", views.schedule_appointment, name="schedule_appointment"),
-    path("my-appointments/", views.my_appointments, name="my_appointments"),
+    # path("schedule/", views.schedule_appointment, name="schedule_appointment"),
+    # path("my-appointments/", views.my_appointments, name="my_appointments"),
 ]
